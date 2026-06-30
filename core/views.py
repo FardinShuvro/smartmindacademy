9a276458b063bc21ade7306668dba58dc4d93bc4
Fardinul Islam Shuvra <fardinulislam2003@gmail.com>
2026-06-30 18:10:02 +0600

    Add Bengali resources and subjects views

--- core/views.py
+++ core/views.py
@@ -12,3 +12,9 @@ def subjects_en(request):
 
 def resources_en(request):
     return render(request, 'resources_en.html')
+
+def subjects_bn(request):
+    return render(request, 'subjects_bn.html')
+
+def resources_bn(request):
+    return render(request, 'resources_bn.html')
